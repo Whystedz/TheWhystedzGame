1f35460195c27f8aecdf1edfda7a12b264a9c14e
Yuguo Zheng <zhenghugo515@gmail.com>
2021-03-18 19:07:22 -0400

    Implement text chat core

--- Assets/Scripts/Vivox/VivoxManager.cs
+++ Assets/Scripts/Vivox/VivoxManager.cs
@@ -1,7 +1,6 @@
 using System;
 using System.ComponentModel;
 using UnityEngine;
-using UnityEngine.UI;
 using VivoxUnity;
 
 namespace Vivox
@@ -16,25 +15,66 @@ namespace Vivox
 
         private static readonly TimeSpan tokenExpiration = TimeSpan.FromSeconds(90);
 
+        // events/delegates
+        public delegate void ChannelTextMessageChangedHandler(string sender,
+            IChannelTextMessage channelTextMessage);
+
+        public event ChannelTextMessageChangedHandler OnTextMessageLogReceivedEvent;
+
+        // vivox client data
         private Client client = new Client();
         private AccountId accountId;
 
         private ILoginSession loginSession;
         public LoginState LoginState { get; private set; }
+
+        // TODO: channelName only for prototyping; create unique channel name once teamManager is setup
         private const string channelName = "sampleChannelName";
+        private ChannelId channelId;
+
+        // maintain single instance
+        private static readonly object myLock = new object();
+        private static VivoxManager instance;
+
+        /// <summary>
+        /// Access singleton instance through this propriety.
+        /// </summary>
+        public static VivoxManager Instance
+        {
+            get
+            {
+                lock (myLock)
+                {
+                    if (instance == null)
+                    {
+                        // Search for existing instance.
+                        instance = (VivoxManager) FindObjectOfType(typeof(VivoxManager));
+
+                        // Create new instance if one doesn't already exist.
+                        if (instance == null)
+                        {
+                            // Need to create a new GameObject to attach the singleton to.
+                            var singletonObject = new GameObject();
+                            instance = singletonObject.AddComponent<VivoxManager>();
+                            singletonObject.name = typeof(VivoxManager) + " (Singleton)";
+                        }
+                    }
+
+                    // Make instance persistent even if its already in the scene
+                    DontDestroyOnLoad(instance.gameObject);
+                    return instance;
+                }
+            }
+        }
 
         private void Awake()
         {
-            // vivoxVoiceManager = VivoxVoiceManager.Instance;
-            client.Uninitialize();
-            client.Initialize();
+            this.client.Uninitialize();
+            this.client.Initialize();
             DontDestroyOnLoad(this);
         }
 
-        private void OnApplicationQuit()
-        {
-            client.Uninitialize();
-        }
+        private void OnApplicationQuit() => this.client.Uninitialize();
 
         #region Login Methods
 
@@ -43,22 +83,22 @@ namespace Vivox
         /// </summary>
         public void LogIn()
         {
-            string uniqueId = Guid.NewGuid().ToString();
+            var uniqueId = Guid.NewGuid().ToString();
             // TODO: for proto purposes only, need to get a real token from server eventually
-            accountId = new AccountId(issuer, uniqueId, tokenDomain);
-            loginSession = client.GetLoginSession(accountId);
-            loginSession.PropertyChanged += OnLoginSessionPropertyChanged;
-            loginSession.BeginLogin(serverUri,
-                loginSession.GetLoginToken(secretKey, tokenExpiration),
+            this.accountId = new AccountId(issuer, uniqueId, tokenDomain);
+            this.loginSession = this.client.GetLoginSession(this.accountId);
+            this.loginSession.PropertyChanged += OnLoginSessionPropertyChanged;
+            this.loginSession.BeginLogin(serverUri,
+                this.loginSession.GetLoginToken(secretKey, tokenExpiration),
                 asyncResult =>
                 {
                     try
                     {
-                        loginSession.EndLogin(asyncResult);
+                        this.loginSession.EndLogin(asyncResult);
                     }
                     catch (Exception e)
                     {
-                        loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
+                        this.loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
                         Debug.LogError(e.Message);
                     }
                 });
@@ -66,11 +106,11 @@ namespace Vivox
 
         public void LogOut()
         {
-            if (loginSession == null || LoginState == LoginState.LoggedOut ||
+            if (this.loginSession == null || LoginState == LoginState.LoggedOut ||
                 LoginState == LoginState.LoggingOut) return;
             // OnUserLoggedOutEvent?.Invoke();
-            loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
-            loginSession.Logout();
+            this.loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
+            this.loginSession.Logout();
         }
 
         // callback on login state changed
@@ -92,7 +132,7 @@ namespace Vivox
                     break;
                 case LoginState.LoggedOut:
                     Debug.Log("[Vivox] Logged out. ");
-                    loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
+                    this.loginSession.PropertyChanged -= OnLoginSessionPropertyChanged;
                     break;
                 default:
                     throw new ArgumentOutOfRangeException();
@@ -106,18 +146,17 @@ namespace Vivox
         /// <summary>
         /// Join a channel
         /// </summary>
-        /// <param name="channelName">channel name</param>
         /// <param name="isConnectAudio">whether to connect audio</param>
         /// <param name="isConnectText">whether to connect text</param>
         /// <param name="channelType">the type of channel</param>
-        public void JoinChannel(string channelName, bool isConnectAudio, bool isConnectText,
-            ChannelType channelType)
+        public void JoinChannel(bool isConnectAudio, bool isConnectText, ChannelType channelType)
         {
             if (LoginState == LoginState.LoggedIn)
             {
-                var channelId = new ChannelId(issuer, channelName, tokenDomain, channelType);
-                var channelSession = loginSession.GetChannelSession(channelId);
+                this.channelId = new ChannelId(issuer, channelName, tokenDomain, channelType);
+                var channelSession = this.loginSession.GetChannelSession(this.channelId);
                 channelSession.PropertyChanged += OnChannelSessionPropertyChanged;
+                channelSession.MessageLog.AfterItemAdded += OnMessageLogReceived;
                 channelSession.BeginConnect(isConnectAudio, isConnectText, true,
                     channelSession.GetConnectToken(secretKey, tokenExpiration),
                     asyncResult =>
@@ -139,10 +178,7 @@ namespace Vivox
             }
         }
 
-        public void LeaveChannel(IChannelSession channelSession)
-        {
-            channelSession.Disconnect();
-        }
+        public void LeaveChannel(IChannelSession channelSession) => channelSession.Disconnect();
 
         private void OnChannelSessionPropertyChanged(object sender,
             PropertyChangedEventArgs propertyChangedEventArgs)
@@ -167,18 +203,45 @@ namespace Vivox
             }
         }
 
+        private void OnMessageLogReceived(object sender,
+            QueueItemAddedEventArgs<IChannelTextMessage> textMessage)
+        {
+            OnTextMessageLogReceivedEvent?.Invoke(textMessage.Value.Sender.DisplayName,
+                textMessage.Value);
+        }
+
         #endregion
 
+        public void SendTextMessage(string messageToSend)
+        {
+            if (ChannelId.IsNullOrEmpty(this.channelId))
+            {
+                throw new ArgumentException("Must provide a valid ChannelId");
+            }
+
+            if (string.IsNullOrEmpty(messageToSend))
+            {
+                throw new ArgumentException("Must provide a message to send");
+            }
+
+            var channelSession = this.loginSession.GetChannelSession(this.channelId);
+            channelSession.BeginSendText(messageToSend,
+                asyncResult =>
+                {
+                    try
+                    {
+                        channelSession.EndSendText(asyncResult);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log($"SendTextMessage failed with exception {e.Message}");
+                    }
+                });
+        }
 
         // bind with UI Login Button
-        public void BtnLogin()
-        {
-            LogIn();
-        }
+        public void BtnLogin() => LogIn();
 
-        public void BtnJoinChannel()
-        {
-            JoinChannel(channelName, true, false, ChannelType.NonPositional);
-        }
+        public void BtnJoinChannel() => JoinChannel(true, true, ChannelType.NonPositional);
     }
 }
