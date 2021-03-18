00815f58d4d2855125e5e4ab1e2f56a0a8ee94d7
Elizabeth Lam <animew.cccl@gmail.com>
2021-03-17 22:25:19 -0400

    Make players connect and spawn on localhost

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -2,20 +2,6 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
-    /* To use InputManager include the following in your script, along with the InputManager prefab in your scene:
-     
-    private InputManager InputManager;
-        
-    private void Start()
-        {
-            InputManager = InputManager.Instance;
-        }
-
-    example call:
-    Vector3 movementInput = new Vector3(InputManager.GetInputMovement().x, 0f, InputManager.GetInputMovement().y);
-
-    */
-
     private static InputManager instance;
 
     private PlayerInput playerInput;
@@ -24,21 +10,17 @@ public class InputManager : MonoBehaviour
     public Vector2 GetInputCamera() => this.playerInput.PlayerControls.Camera.ReadValue<Vector2>();
     public Vector2 GetInputMovement() => this.playerInput.PlayerControls.Movement.ReadValue<Vector2>();
     public bool GetDigging() => this.playerInput.PlayerControls.Digging.triggered;
+    public PlayerInput GetPlayerInput() => this.playerInput;
+    
     private void Awake()
     {
         MaintainSingleInstance();
         this.playerInput = new PlayerInput();
     }
 
-    private void OnEnable()
-    {
-        this.playerInput.Enable();
-    }
-
-    private void OnDisable()
-    {
-        this.playerInput.Disable();
-    }
+    private void OnEnable() => this.playerInput.Enable();
+    private void OnDisable() => this.playerInput.Disable();
+    private void OnDestroy() => this.playerInput = null;
 
     private void MaintainSingleInstance()
     {
@@ -47,5 +29,4 @@ public class InputManager : MonoBehaviour
         else
             instance = this;
     }
-
 }
---
+++ Assets/Scripts/NetworkPlayer/NetworkDigging.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Mirror;
+
+public class NetworkDigging : NetworkBehaviour
+{
+
+    [SerializeField] private float minDistanceToDiggableTile = 1.0f;
+    [SerializeField] private float maxDistanceToDiggableTile = 1.0f;
+    private InputManager inputManager;
+    private int layerMask;
+    private PlayerMovement playerMovement;
+
+    [SerializeField] private bool enableDebugMode = true;
+
+    private void Awake()
+    {
+        layerMask = LayerMask.GetMask("TileMovementCollider");
+        playerMovement = this.GetComponent<PlayerMovement>();
+    }
+    void Start() => inputManager = InputManager.GetInstance();
+
+    void Update()
+    {
+        if (base.hasAuthority)
+        {
+            if (playerMovement.IsFalling())
+                return;
+
+            var hits = Physics.RaycastAll(transform.position,
+                transform.TransformDirection(Vector3.forward),
+                this.maxDistanceToDiggableTile,
+                layerMask
+                );
+
+            if (hits.Length == 0)
+                return;
+
+            var closestCollider = GetClosestCollider(hits);
+
+            if (closestCollider == null)
+                return;
+
+            var hitGameObject = closestCollider.transform.parent.gameObject;
+
+            var tile = hitGameObject.GetComponent<Tile>();
+            if (tile.tileState != TileState.Normal)
+                return;
+
+            StartCoroutine(tile.HighlightTile());
+
+            if (inputManager.GetDigging())
+                tile.DigTile();
+        }
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if (enableDebugMode)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(
+                Vector3.right * 0.1f + transform.position,
+                Vector3.right * 0.1f + transform.position + transform.forward * this.minDistanceToDiggableTile
+            );
+
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(
+                transform.position,
+                transform.position + transform.forward * this.maxDistanceToDiggableTile
+            );
+        }
+    }
+
+    private Collider GetClosestCollider(RaycastHit[] hits)
+    {
+        var closestDistnace = Mathf.Infinity;
+        Collider closestCollider = null;
+
+        int hitsLength = hits.Length;
+        for (int i = 0; i < hitsLength; i++)
+        {
+            var distance = Vector3.Distance(transform.position, hits[i].collider.transform.position);
+            if (distance < this.minDistanceToDiggableTile)
+                continue; // ignore tiles that are too close
+
+            if (distance < closestDistnace)
+            {
+                closestCollider = hits[i].collider;
+                closestDistnace = distance;
+            }
+        }
+
+        return closestCollider;
+    }
+}
