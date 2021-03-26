f5d292e019486edc8747dc8afc77339ee07831c1
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2021-03-25 20:58:07 -0400

    Focus on Triangle Combo

--- Assets/Scripts/Combos/ComboManager.cs
+++ Assets/Scripts/Combos/ComboManager.cs
@@ -7,45 +7,21 @@ using UnityEngine;
 
 public class ComboManager : MonoBehaviour
 {
-    [Range(5f, 50f)]
-    [SerializeField] private float distanceCombo2;
-    public float DistanceCombo2 { get => this.distanceCombo2; }
+    [SerializeField] private float lineDistance;
+    [SerializeField] private float lineTolerance;
     
-    [Range(0, 10f)]
-    [SerializeField] private float triggerToleranceCombo2;
-    public float TriggerToleranceCombo2 { get => this.triggerToleranceCombo2; }
-
-    [Range(5f, 50f)]
-    [SerializeField] private float distanceCombo3;
-    public float DistanceCombo3 { get => this.distanceCombo3; }
+    [SerializeField] private float triangleDistance;
+    [SerializeField] private float triangleTolerance;
     
-    [Range(0, 10f)]
-    [SerializeField] private float triggerToleranceCombo3;
-    public float TriggerToleranceCombo3 { get => this.triggerToleranceCombo3; }
-
-    [Range(5f, 50f)]
-    [SerializeField] private float distanceCombo4;
-    public float DistanceCombo4 { get => this.distanceCombo4; }
+    [SerializeField] private float squareDistance;
+    [SerializeField] private float squareTolerance;
     
-    [Range(0, 10f)]
-    [SerializeField] private float triggerToleranceCombo4;
-    public float TriggerToleranceCombo4 { get => this.triggerToleranceCombo4; }
-
-    [Range(0, 50f)]
-    [SerializeField] private float highlightingTolerance;
-    public float HighlightingTolerance { get => this.highlightingTolerance; }
+    [SerializeField] private float highlightTolerance;
 
     [SerializeField] private Color hintColor;
-    public Color HintColor { get => this.hintColor; }
-    [SerializeField] private Color combo2Color;
-    public Color Combo2Color { get => this.combo2Color; }
-    [SerializeField] private Color combo3Color;
-    public Color Combo3Color { get => this.combo3Color; }
-    [SerializeField] private Color combo4Color;
-    public Color Combo4Color { get => this.combo4Color; }
-    
-    // https://www.calculator.net/triangle-calculator.html?vc=120&vx=&vy=&va=30&vz=1&vb=30&angleunits=d&x=55&y=8
-    public const float ComboRadiusModifier = 0.57735f;
+    [SerializeField] private Color lineColor;
+    [SerializeField] private Color triangleColor;
+    [SerializeField] private Color squareColor;
 
     private List<ComboPlayer> players;
 
@@ -56,17 +32,79 @@ public class ComboManager : MonoBehaviour
 
     private void Update()
     {
-        CheckAllTriangleCombos();
-    }
-
-    private void CheckAllTriangleCombos()
-    {
         foreach (var player in this.players)
-            CheckTriangleCombo(player);
+            CheckTriangleCombosForPlayer(player);
     }
 
-    private void CheckTriangleCombo(ComboPlayer player)
+    private void CheckTriangleCombosForPlayer(ComboPlayer player)
     {
-        //var team = player.GetComponent<TeamPlayer>().
+        var teammates = player.Teammates.ToArray();
+
+        // Narrow down by cooldown
+        teammates = teammates
+            .Where(player => !player.IsOnCooldown)
+            .ToArray();
+        if (teammates.Count() == 0)
+            return;
+
+        // Narrow down by max distance -- if we cannot highlight, consider it out of max distance
+        teammates = teammates
+            .Where(teammate => IsWithinRange(player, teammate, this.triangleDistance, this.highlightTolerance))
+            .ToArray();
+        if (teammates.Count() < 2)
+            return;
+
+        CheckTriangleCombo(player, teammates[0], teammates[1]);
+
+        // With 4 players, a second set of 3 may be possible involving this player:
+        if (teammates.Count() >= 3)
+            CheckTriangleCombo(player, teammates[1], teammates[2]);
+    }
+
+    private void CheckTriangleCombo(ComboPlayer a, ComboPlayer b, ComboPlayer c)
+    {
+        // If we made it here, then it is at least in bounds for highlighting the combo
+
+        // Let's see if we can trigger it
+        if (IsWithinRange(a, b, this.triangleDistance, this.triangleTolerance)
+            && IsWithinRange(a, c, this.triangleDistance, this.triangleTolerance)
+            && IsWithinRange(b, c, this.triangleDistance, this.triangleTolerance))
+        {
+            HighlightTriggerableTriangleCombo(a, b, c);
+            return;
+        }
+
+        HighlightTriangleComboWithHint(a, b, c);
+    }
+
+    private void HighlightTriangleComboWithHint(ComboPlayer a, ComboPlayer b, ComboPlayer c)
+    {
+        //Debug.Log("Hint");
+        Highlight(a, b, this.hintColor);
+        Highlight(a, c, this.hintColor);
+        Highlight(b, c, this.hintColor);
+    }
+
+    private void HighlightTriggerableTriangleCombo(ComboPlayer a, ComboPlayer b, ComboPlayer c)
+    {
+        //Debug.Log($"Triangle Combo between {this.name}, {b.name}, and {c.name}!");
+        Highlight(a, b, this.triangleColor);
+        Highlight(a, c, this.triangleColor);
+        Highlight(b, c, this.triangleColor);
+    }
+
+    private void Highlight(ComboPlayer a, ComboPlayer b, Color color)
+    {
+        Debug.DrawLine(a.transform.position, b.transform.position, color);
+    }
+
+    private bool IsWithinRange(ComboPlayer a, ComboPlayer b, float requiredDistance, float tolerance)
+    {
+        var distance = Vector3.Distance(a.transform.position, b.transform.position);
+
+        var lowerBound = requiredDistance - tolerance;
+        var upperBound = requiredDistance + tolerance;
+
+        return distance >= lowerBound && distance <= upperBound;
     }
 }
--- Assets/Scripts/Combos/ComboPlayer.cs
+++ Assets/Scripts/Combos/ComboPlayer.cs
@@ -25,6 +25,19 @@ public class ComboPlayer : MonoBehaviour
     private GameObject[,] debugSpheresCombo4;
     private GameObject[,] debugSpheresCombo3;
 
+    private List<ComboPlayer> teammates;
+    public List<ComboPlayer> Teammates { get => this.teammates; }
+
+    private void Awake()
+    {
+        var team = this.GetComponent<Teammate>().Team;
+
+        this.teammates = FindObjectsOfType<Teammate>()
+            .Where(teammate => teammate.Team == team && teammate != this)
+            .Select(teammate => teammate.GetComponent<ComboPlayer>())
+            .ToList();
+    }
+
     private void InstantiateAllDebugSpheres()
     {
         this.debugSpheresCombo4 = new GameObject[3, 4];
@@ -69,182 +82,12 @@ public class ComboPlayer : MonoBehaviour
     {
         //DeactivateAllDebugSpheres();
 
-        //CooldownUpdate();
+        CooldownUpdate();
 
         //Check4Combos();  
         //Check3Combos();  
         //Check2Combos();
     }
-
-    private void LateUpdate()
-    {
-        IsInvolvedInACombo4 = false;
-        IsInvolvedInACombo3 = false;
-    }
-
-    private void Check4Combos()
-    {
-        // Narrow down by cooldown
-        var applicableTeamMembers = this.team.Where(player => !player.IsOnCooldown).ToList();
-        if (applicableTeamMembers.Count() == 0)
-            return;
-
-        // Narrow down by max distance -- highlighting will be the max
-        applicableTeamMembers = applicableTeamMembers.Where(player =>
-            IsInRange(this, player, comboManager.DistanceCombo3, comboManager.HighlightingTolerance)
-        ).ToList();
-        if (applicableTeamMembers.Count() < 3)
-            return;
-
-        var a = this;
-        var b = applicableTeamMembers[0];
-        var c = applicableTeamMembers[1];
-        var d = applicableTeamMembers[2];
-
-        if (!IsInRange(a, b, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        if (!IsInRange(a, c, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        if (!IsInRange(a, d, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        if (!IsInRange(b, c, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        if (!IsInRange(b, d, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        if (!IsInRange(c, d, comboManager.DistanceCombo4, comboManager.TriggerToleranceCombo4))
-            return;
-
-        Debug.Log($"Epic 4 Combo between {this.name}, {b.name}, {c.name}, and {d.name}!");
-        
-        Highlight(a, b, this.comboManager.Combo4Color);
-        Highlight(a, c, this.comboManager.Combo4Color);
-        Highlight(a, d, this.comboManager.Combo4Color);
-        Highlight(b, c, this.comboManager.Combo4Color);
-        Highlight(b, d, this.comboManager.Combo4Color);
-        Highlight(c, d, this.comboManager.Combo4Color);
-        
-        this.IsInvolvedInACombo4 = true;
-    }
-
-    private void Check3Combos()
-    {
-        // Narrow down by cooldown
-        var applicableTeamMembers = this.team.Where(player => !player.IsOnCooldown).ToList();
-        if (applicableTeamMembers.Count() == 0)
-            return;
-
-        // TODO update this
-        // Narrow down by max distance -- highlighting will be the max
-        applicableTeamMembers = applicableTeamMembers.Where(player =>
-            IsInRange(this, player, comboManager.DistanceCombo3, comboManager.HighlightingTolerance)
-        ).ToList();
-        if (applicableTeamMembers.Count() < 2)
-            return;
-
-        Check3Combo(applicableTeamMembers[0], applicableTeamMembers[1], 0);
-
-        // With 4 players, a second set of 3 may be possible involving this player:
-        if (applicableTeamMembers.Count() >= 3)
-            Check3Combo(applicableTeamMembers[1], applicableTeamMembers[2], 1);
-    }
-
-    private void Check3Combo(ComboPlayer b, ComboPlayer c, int setIndex)
-    {
-        var a = this; // for the sake of consistency with b and c
-
-        var centerOfMass = (a.transform.position + b.transform.position + c.transform.position) / 3;
-        var directionToCenter = (centerOfMass - this.transform.position).normalized;
-        var centerOfComboDircle = a.transform.position + directionToCenter * ComboManager.ComboRadiusModifier * this.comboManager.DistanceCombo3;
-
-        Debug.DrawLine(
-            a.transform.position,
-            centerOfComboDircle, 
-            Color.grey);
-
-        var comboSphere = this.debugSpheresCombo3[setIndex, 0];
-        comboSphere.SetActive(true);
-        comboSphere.transform.position = centerOfComboDircle;
-        var comboSphereRadius = ComboManager.ComboRadiusModifier * this.comboManager.DistanceCombo3;
-        comboSphere.transform.localScale = 2 * Vector3.one * comboSphereRadius;
-
-        var toleranceSphereB = this.debugSpheresCombo3[setIndex, 1];
-        toleranceSphereB.SetActive(true);
-        toleranceSphereB.transform.position = centerOfComboDircle 
-            + Quaternion.AngleAxis(-60f, Vector3.up) * directionToCenter * comboSphereRadius;
-        toleranceSphereB.transform.localScale = 2 * Vector3.one * this.comboManager.TriggerToleranceCombo3;
-
-        var toleranceSphereC = this.debugSpheresCombo3[setIndex, 2];
-        toleranceSphereC.SetActive(true);
-        toleranceSphereC.transform.position = centerOfComboDircle
-            + Quaternion.AngleAxis(60f, Vector3.up) * directionToCenter * comboSphereRadius;
-        toleranceSphereC.transform.localScale = 2 * Vector3.one * this.comboManager.TriggerToleranceCombo3;
-
-        if (!IsInRange(a, b, comboManager.DistanceCombo3, comboManager.TriggerToleranceCombo3))
-            return;
-
-        if (!IsInRange(a, c, comboManager.DistanceCombo3, comboManager.TriggerToleranceCombo3))
-            return;
-
-        if (!IsInRange(b, c, comboManager.DistanceCombo3, comboManager.TriggerToleranceCombo3))
-            return;
-
-        Debug.Log($"3 Combo between {this.name}, {b.name}, and {c.name}!");
-
-        this.IsInvolvedInACombo3 = true;
-
-        if (!this.IsInvolvedInACombo4)
-        {
-            Highlight(a, b, this.comboManager.Combo3Color);
-            Highlight(a, c, this.comboManager.Combo3Color);
-            Highlight(b, c, this.comboManager.Combo3Color);
-        }
-    }
-
-    private void Check2Combos()
-    {
-        // Narrow down by cooldown
-        var applicableTeamMembers = this.team.Where(player => !player.IsOnCooldown);
-        if (applicableTeamMembers.Count() == 0)
-            return;
-
-        // Narrow down by max distance -- highlighting will be the max
-        applicableTeamMembers = applicableTeamMembers.Where(player => 
-            IsInRange(this, player, comboManager.DistanceCombo2, comboManager.HighlightingTolerance)
-        );
-        if (applicableTeamMembers.Count() == 0)
-            return;
-
-        foreach (var player in applicableTeamMembers)
-        {
-            var canTrigger = IsInRange(this, player, this.comboManager.DistanceCombo2, this.comboManager.TriggerToleranceCombo2);
-            var color = canTrigger ? this.comboManager.Combo2Color : this.comboManager.HintColor;
-
-            if (!this.IsInvolvedInACombo3 && !this.IsInvolvedInACombo4)
-                Highlight(this, player, color);
-        }
-
-    }
-
-    private void Highlight(ComboPlayer a, ComboPlayer b, Color color)
-    {
-        //Debug.DrawLine(a.transform.position, b.transform.position, color);
-    }
-
-    private bool IsInRange(ComboPlayer a, ComboPlayer b, float requiredDistance, float tolerance)
-    {
-        var distance = Vector3.Distance(a.transform.position, b.transform.position);
-
-        var lowerBound = requiredDistance - tolerance;
-        var upperBound = requiredDistance + tolerance;
-
-        return distance >= lowerBound && distance <= upperBound;
-    }
-
     private void StartCooldown()
     {
         this.IsOnCooldown = true;
