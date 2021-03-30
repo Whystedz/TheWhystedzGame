34c0724430a670633346bcd97abd2e535c68262f
Wayne Huras <wayne.stamand@hotmail.com>
2021-03-30 18:33:06 -0400

    Add controls for rope, combo, mute self, and main menu

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,6 +10,10 @@ public class InputManager : MonoBehaviour
     public Vector2 GetInputCamera() => this.playerInput.PlayerControls.Camera.ReadValue<Vector2>();
     public Vector2 GetInputMovement() => this.playerInput.PlayerControls.Movement.ReadValue<Vector2>();
     public bool GetDigging() => this.playerInput.PlayerControls.Digging.triggered;
+    public bool GetRope() => this.playerInput.PlayerControls.DropRope.triggered;
+    public bool GetMuteSelf() => this.playerInput.PlayerControls.MuteSelf.triggered;
+    public bool GetInitiateCombo() => this.playerInput.PlayerControls.InitiateCombo.triggered;
+    public bool GetMainMenu() => this.playerInput.PlayerControls.MainMenu.triggered;
     public PlayerInput GetPlayerInput() => this.playerInput;
     
     private void Awake()
