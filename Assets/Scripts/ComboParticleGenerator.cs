edd6379ee376d3ee0b02a6ed00455836ebd7e3f8
Wayne Huras <wayne.stamand@hotmail.com>
2021-04-03 21:23:11 -0400

    Add Combo Indicators - Particle Effects

---
+++ Assets/Scripts/ComboParticleGenerator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComboParticleGenerator : MonoBehaviour
+{
+    private ComboPlayer comboPlayer;
+    private List<ComboPlayer> teammates;
+
+    [SerializeField] private GameObject particleConnectionPrefab;
+
+    private void Start()
+    {
+        this.comboPlayer = transform.parent.GetComponent<ComboPlayer>();
+        this.teammates = this.comboPlayer.Teammates(false);
+
+        for(int i = 0; i < this.teammates.Count; i++)
+        {
+            Instantiate(this.particleConnectionPrefab, transform)
+                .GetComponent<ComboParticleIndicator>()
+                .SetPlayers(this.comboPlayer, this.teammates[i].GetComponent<ComboPlayer>());
+            
+            /* TODO: Nice to have - complete the particle effect logic for triangle combos - lines originating from the other two participants
+            for (int j = 0; j < this.teammates.Count; j++)
+            {
+                if(this.teammates[i] != this.teammates[j])
+                    Instantiate(this.particleConnectionPrefab, transform).GetComponent<ParticleConnection>().SetPlayers(this.teammates[i].GetComponent<Transform>(), this.teammates[j].GetComponent<Transform>());
+            }*/
+        }
+    }
+
+
+}
