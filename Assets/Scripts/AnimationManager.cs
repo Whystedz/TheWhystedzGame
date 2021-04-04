3c79901f056363bab95b53466be7708b8b74e040
whuras <46137966+whuras@users.noreply.github.com>
2021-04-03 21:09:32 -0400

    Revert "Update Animator to handle underground reset"
    This reverts commit b4f2fd06a46e4f0eec173e3358ca07cc567c0d12.

--- Assets/Scripts/AnimationManager.cs
+++ Assets/Scripts/AnimationManager.cs
@@ -26,14 +26,7 @@ public class AnimationManager : MonoBehaviour
 
     void Update()
     {
-        if (!this.playerMovement.IsInUnderground && (animator.GetCurrentAnimatorStateInfo(0).IsName("IdleUnderGround") || animator.GetCurrentAnimatorStateInfo(0).IsName("RunningUnderGround")))
-        {
-            this.animator.SetTrigger("Reset");
-            Debug.LogError("Checkerino");
-        }
-            
-
-        if (this.playerMovement.IsFalling() && !this.fallingTrigger && this.playerMovement.IsInUnderground)
+        if (this.playerMovement.IsFalling() && !this.fallingTrigger)
         {
             this.fallingTrigger = true;
             this.animator.SetTrigger("Fall");
