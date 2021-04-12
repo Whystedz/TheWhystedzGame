7e81762e921a7cea51994c850883cf2ac4a5c034
Shifat Khan <shifatkhan6@gmail.com>
2021-04-11 23:43:55 -0400

    Update End screen UI

--- Assets/Scripts/Game/EndScreenUI.cs
+++ Assets/Scripts/Game/EndScreenUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndScreenUI : MonoBehaviour
 {
@@ -17,7 +18,20 @@ public class EndScreenUI : MonoBehaviour
 
     [SerializeField] private string teamRedName;
     [SerializeField] private string teamBlueName;
-    
+
+    [SerializeField] private GameObject teamRedScoresGUI;
+    [SerializeField] private GameObject teamBlueScoresGUI;
+
+    [Header("LeafyBoy Images")] 
+    [SerializeField] private Image leafyBoyLeftImg;
+    [SerializeField] private Image leafyBoyRightImg;
+    [SerializeField] private Sprite[] teamRedLeafyBoysSprites;
+    [SerializeField] private Sprite[] teamBlueLeafyBoysSprites;
+    [SerializeField] private Image teamBG;
+    [SerializeField] private Sprite teamRedBG;
+    [SerializeField] private Sprite teamBlueBG;
+
+
     public void EndGame()
     {
         ShowEndScreen();
@@ -26,20 +40,53 @@ public class EndScreenUI : MonoBehaviour
         if (this.redTeam.Score == this.blueTeam.Score)
         {
             this.winningTeamTxt.text = $"TIE";
+            this.leafyBoyLeftImg.sprite = this.teamRedLeafyBoysSprites[0];
+            this.leafyBoyRightImg.sprite = this.teamBlueLeafyBoysSprites[1];
             return;
         }
         
         // Update UI.
         bool teamRedWon = this.redTeam.Score > this.blueTeam.Score;
         Team winningTeam = teamRedWon ? Team.RedTeam : Team.BlueTeam;
-        string teamName = teamRedWon ? teamRedName : teamBlueName;
+        string teamName = teamRedWon ? this.teamRedName : this.teamBlueName;
         
         string hexColor = ColorUtility.ToHtmlStringRGB
             (winningTeam == Team.RedTeam ? this.teamRedColor : this.teamBlueColor);
-        this.winningTeamTxt.text = $"Team <color=#{hexColor}>{teamName}</color> Won!";
+        this.winningTeamTxt.text = $"Team <color=#{hexColor}>{teamName}</color> won!!";
+
+        ChangeLeafyBoyImage(winningTeam);
+    }
+    
+    private void ChangeLeafyBoyImage(Team winnerTeam)
+    {
+        if (winnerTeam == Team.RedTeam)
+        {
+            this.teamBG.sprite = this.teamRedBG;
+            
+            this.leafyBoyLeftImg.sprite = this.teamRedLeafyBoysSprites[0];
+            this.leafyBoyRightImg.sprite = this.teamRedLeafyBoysSprites[1];
+
+            this.redTeam.gameObject.SetActive(true);
+            this.blueTeam.gameObject.SetActive(false);
+            this.teamRedScoresGUI.SetActive(true);
+            this.teamBlueScoresGUI.SetActive(false);
+        }
+        else
+        {
+            this.teamBG.sprite = this.teamBlueBG;
+
+            this.leafyBoyLeftImg.sprite = this.teamBlueLeafyBoysSprites[0];
+            this.leafyBoyRightImg.sprite = this.teamBlueLeafyBoysSprites[1];
+            
+            this.redTeam.gameObject.SetActive(false);
+            this.blueTeam.gameObject.SetActive(true);
+            this.teamRedScoresGUI.SetActive(false);
+            this.teamBlueScoresGUI.SetActive(true);
+        }
     }
     
     public void ShowEndScreen() => this.endScreen.SetActive(true);
     
+    // TODO: Return to lobby as well.
     public void CloseEndScreen() => this.endScreen.SetActive(false);
 }
